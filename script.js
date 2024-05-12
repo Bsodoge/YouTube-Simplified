85cea884a21c0f7cc17cd611ff1da08c17a2b165
bsodoge <kashhh4u@gmail.com>
2024-05-12 20:28:53 +0100

    Display correct browser logo

---
+++ script.js
@@ -0,0 +1,31 @@
+const getExtensionLink = document.getElementById("extension_link");
+const getExtensionImage = document.getElementById("extension_image");
+
+const browserLinks = {
+    firefox: "firefox",
+    edge: "edge",
+    opera: "opera",
+    chrome: "chrome"
+}
+
+const browserImages = {
+    firefox: "/images/firefox.svg",
+    edge: "/images/edge.svg",
+    opera: "opera",
+    chrome: "/images/chrome.svg"
+}
+
+const browser = (function() {
+    const test = regexp => regexp.test(window.navigator.userAgent);
+    switch (true) {
+        case test(/edg/i): "edge";
+        case test(/firefox|fxios/i): return "firefox";
+        case test(/opr\//i): return "opera";
+        case test(/chrome|chromium|crios/i): return "chrome";
+        default: return "chrome";
+    }
+})();
+
+getExtensionLink.href = browserLinks[browser];
+getExtensionImage.src = browserImages[browser];
+
